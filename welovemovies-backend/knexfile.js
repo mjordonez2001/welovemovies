2f2504ada7ef3ee3f8f10ff714b61f04dbbe0ab2
Maria Ordonez <ordonezyuyis@gmail.com>
2022-05-30 21:35:19 -0500

    added db url production to knexfile

--- welovemovies-backend/knexfile.js
+++ welovemovies-backend/knexfile.js
@@ -3,13 +3,14 @@ const path = require("path");
 require("dotenv").config();
 
 const {
-  DATABASE_URL = "postgresql://postgres@localhost/postgres",
+  DATABASE_URL_DEVELOPMENT = "postgresql://postgres@localhost/postgres",
+  DATABASE_URL_PRODUCTION = "postgresql://postgres@localhost/postgres"
 } = process.env;
 
 module.exports = {
   development: {
     client: "postgresql",
-    connection: DATABASE_URL,
+    connection: DATABASE_URL_DEVELOPMENT,
     pool: { min: 0, max: 5 },
     migrations: {
       directory: path.join(__dirname, "src", "db", "migrations"),
@@ -21,7 +22,7 @@ module.exports = {
 
   production: {
     client: "postgresql",
-    connection: DATABASE_URL,
+    connection: DATABASE_URL_PRODUCTION,
     pool: { min: 0, max: 5 },
     migrations: {
       directory: path.join(__dirname, "src", "db", "migrations"),
