16182ec1ea52c7b2271ff20449f56ddff16a12ad
Maria Ordonez <ordonezyuyis@gmail.com>
2022-06-01 16:28:51 -0500

    adding backend url to api

--- welovemovies-frontend/src/utils/api.js
+++ welovemovies-frontend/src/utils/api.js
@@ -1,5 +1,4 @@
-const API_BASE_URL =
-  process.env.REACT_APP_API_BASE_URL || "http://localhost:5000";
+const API_BASE_URL = "https://mo-welovemovies-backend.herokuapp.com/";
 
 /**
  * Defines the default headers for these functions to work with `json-server`
