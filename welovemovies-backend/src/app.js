3846f538449902bd9c9934030748f99fa8dd03d9
Maria Ordonez <ordonezyuyis@gmail.com>
2022-06-01 16:36:13 -0500

    moving up cors in app.js

--- welovemovies-backend/src/app.js
+++ welovemovies-backend/src/app.js
@@ -7,8 +7,8 @@ const moviesRouter = require("./routes/movies/movies.router");
 const reviewsRouter = require("./routes/reviews/reviews.router");
 const theatersRouter = require("./routes/theaters/theaters.router");
 
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
 
 app.use("/movies", moviesRouter);
 app.use("/reviews", reviewsRouter);
